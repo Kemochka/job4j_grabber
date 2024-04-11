36e07514c46f0328ac19c96b92386d719be2b801
kemochka <kemaevak@yandex.ru>
2024-04-11 20:20:59 +0300

    2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,47 +5,64 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
     private static final String SOURCE_LINK = "https://career.habr.com";
-    public static final String PREFIX = "/vacancies?page=";
-    public static final String SUFFIX = "&q=Java%20developer&type=all";
     public static final int NUM_PAGES = 5;
-    public static String description;
+    private final DateTimeParser dateTimeParser;
 
-    public static void main(String[] args) throws IOException {
-        for (int i = 1; i <= NUM_PAGES; i++) {
-            String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, i, SUFFIX);
-            Connection connection = Jsoup.connect(fullLink);
-            Document document = connection.get();
-            Elements rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
-                Element titleElement = row.select(".vacancy-card__title").first();
-                Element linkElement = titleElement.child(0);
-                String vacancyName = titleElement.text();
-                Element vacancyDate = row.select(".vacancy-card__date").first();
-                Element date = vacancyDate.child(0);
-                String vacDate = date.attr("datetime");
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                String descriptions;
-                try {
-                    descriptions = retrieveDescription(link);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.printf("%s %s %s%n %s%n", vacancyName, vacDate, link, descriptions);
-            });
-        }
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
     }
-    private static String retrieveDescription(String link) throws IOException {
-     Connection connection = Jsoup.connect(link);
-     Document document = connection.get();
-     Elements rows = document.select(".faded-content__container");
-     rows.forEach(row -> {
-                 Element titleElement = row.select(".faded-content").first();
-                 description = titleElement.text();
-             });
-     return description;
+
+    private static String retrieveDescription(String link) {
+        String text;
+        try {
+            text = Jsoup.connect(link).get()
+                    .select(".faded-content")
+                    .text();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return text;
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> postList = new ArrayList<>();
+        for (int i = 1; i < NUM_PAGES; i++) {
+            try {
+                Connection connection = Jsoup.connect(String.format("%s?page=%s", link, i));
+                Document document = connection.get();
+                Elements rows = document.select(".vacancy-card__inner");
+                for (Element row : rows) {
+                    postList.add(postParse(row));
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return postList;
+    }
+
+    private Post postParse(Element row) throws IOException {
+        Post post = new Post();
+        post.setTitle(row.select(".vacancy-card__title").first().text());
+        post.setDescription(retrieveDescription(String.format("%s%s", SOURCE_LINK, row.select(".vacancy-card__title")
+                .first().child(0)
+                .attr("href"))));
+        post.setLink(String.format("%s%s", SOURCE_LINK, row.select(".vacancy-card__title")
+                .first()
+                .child(0)
+                .attr("href")));
+        post.setCreated(dateTimeParser.parse(row.select(".vacancy-card__date")
+                .first()
+                .child(0)
+                .attr("datetime")));
+        return post;
     }
 }
--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -4,11 +4,11 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Post {
-    private final int id;
-    private final String title;
-    private final String link;
-    private final String description;
-    private final LocalDateTime created;
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
 
     public Post(int id, String title, String link, String description, LocalDateTime created) {
         this.id = id;
@@ -16,6 +16,9 @@ public class Post {
         this.link = link;
         this.description = description;
         this.created = created;
+    }
+
+    public Post() {
     }
 
     @Override
@@ -45,5 +48,45 @@ public class Post {
                 + ", description='" + description + '\''
                 + ", created=" + created
                 + '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
     }
 }
